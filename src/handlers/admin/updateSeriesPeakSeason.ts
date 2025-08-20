c4dadcbe84f3326569d5c0bb78fa70139479c1b5
aries <aries.dimailig8@gmail.com>
2025-08-20 17:34:31 +0800

    feat: update UpdateSeriesPeakSeasonPayload to update the payoutSchedule too

--- src/handlers/admin/updateSeriesPeakSeason.ts
+++ src/handlers/admin/updateSeriesPeakSeason.ts
@@ -1,15 +1,18 @@
 import { Request, Response } from "express";
 import { prisma } from "../../utils/prisma";
+import { series_payout_schedule } from "@prisma/client";
 
 interface UpdateSeriesPeakSeasonPayload {
     seriesId: string;
     peakSeasonStartMonth: number;
     peakSeasonEndMonth: number;
+    payoutSchedule: string;
 }
 
 export default async function updateSeriesPeakSeason(req: Request, res: Response) {
-    const { seriesId, peakSeasonStartMonth, peakSeasonEndMonth } = req.body as UpdateSeriesPeakSeasonPayload;
+    const { seriesId, peakSeasonStartMonth, peakSeasonEndMonth, payoutSchedule } = req.body as UpdateSeriesPeakSeasonPayload;
 
+    const acceptedPayoutSchedule: series_payout_schedule[] = ["WEEKLY", "MONTHLY", "QUARTERLY"];
     // validate payloads
     if (!seriesId || seriesId.trim() === "") {
         return res.status(400).json({ message: "Invalid series ID." });
@@ -23,6 +26,9 @@ export default async function updateSeriesPeakSeason(req: Request, res: Response
     if (peakSeasonStartMonth > peakSeasonEndMonth) {
         return res.status(400).json({ message: "Peak season start month cannot be after end month." });
     }
+    if (!payoutSchedule || !acceptedPayoutSchedule.includes(payoutSchedule as series_payout_schedule)) {
+        return res.status(400).json({ error: "Invalid payout schedule." });
+    }
 
     try {
         const series = await prisma.series.findUnique({
@@ -34,18 +40,23 @@ export default async function updateSeriesPeakSeason(req: Request, res: Response
             return res.status(404).json({ message: "Series not found." });
         }
 
-        await prisma.series_peak_season.update({
+        await prisma.series.update({
             where: {
-                seriesID: series.id
+                id: series.id
             },
             data: {
-                peakSeasonStartMonth,
-                peakSeasonEndMonth,
+                payoutSchedule: payoutSchedule as series_payout_schedule,
                 updatedAt: new Date(),
+                peakSeason: {
+                    update: {
+                        peakSeasonStartMonth,
+                        peakSeasonEndMonth,
+                        updatedAt: new Date()
+                    }
+                }
             }
         })
-
-        return res.status(200).json({ message: "Series peak season updated." })
+        return res.status(200).json({ message: "Series updated." })
     } catch (error) {
         console.log("Error on updateSeriesPeakSeason: ", error)
         return res.status(500).json({ message: "Internal server error." });
