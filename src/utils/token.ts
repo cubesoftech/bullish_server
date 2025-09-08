5751f91c918694dbaca265b978a136ab6c84557e
aries <aries.dimailig8@gmail.com>
2025-09-08 12:19:57 +0800

    fix: fix token expiration

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -2,12 +2,17 @@ import jwt from "jsonwebtoken"
 import ms from "ms"
 import { AuthPayload } from "./interface"
 
-const Access_TTL = ms("1d");
+const Access_TTL = ms("1d") / 1000; //convert to seconds;
 
 export function signAccessToken(payload: { id: AuthPayload["id"] }) {
+    const accessSecret = process.env.JWT_ACCESS_SECRET;
+    if (!accessSecret) {
+        throw new Error("JWT_ACCESS_SECRET is not defined in environment variables.");
+    }
+
     return jwt.sign(
         payload,
-        process.env.JWT_ACCESS_SECRET!,
+        accessSecret,
         { expiresIn: Access_TTL, algorithm: "HS256" }
     )
 }
