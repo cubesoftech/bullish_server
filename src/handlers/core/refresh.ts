65be45250f7c0aa426cd7120fe084da15f2261b5
aries <aries.dimailig8@gmail.com>
2025-09-29 12:24:38 +0800

    chore: remove console log on refresh access token endpoint

--- src/handlers/core/refresh.ts
+++ src/handlers/core/refresh.ts
@@ -50,7 +50,6 @@ export default async function refresh(req: Request, res: Response) {
 
         return res.status(200).json({ data: { accessToken: newAccessToken, refreshToken: newRefreshToken } });
     } catch (error) {
-        console.error("Error refreshing token:", error);
         return res.status(500).json({ message: "내부 서버 오류." });
     }
 }
