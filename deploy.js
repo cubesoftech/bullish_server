806f2c8dd051368c13b54a7ad4fe471ac569ccd2
aries <aries.dimailig8@gmail.com>
2025-10-29 15:36:38 +0800

    fix: fix server access

--- deploy.js
+++ deploy.js
@@ -1,10 +1,4 @@
-
-const access1 = 'ssh trusseon-server@154.18.187.109'
-const access1_1 = 'ssh trusseon'
-const access2 = 'cd htdocs/server.trusseon.com'
-
-const access11 = 'ssh trusseonglobal-server@154.18.187.109'
-const access11_1 = 'ssh trusseonglobal'
-const access21 = 'cd htdocs/server.trusseonglobal.com'
+const access1 = 'ssh bullish-korea-server@154.18.187.109'
+const access2 = 'cd htdocs/server.bullish-korea.com'
 
 const run = "npm run build && pm2 delete all && pm2 start npm --name server -- start && pm2 logs --lines 1000"
